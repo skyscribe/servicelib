bfe01395499a6c3eb51e022f4b9e7fea6ce11cbc
Fei <fei.yan@nokia.com>
2015-11-17 16:18:16 +0800

    fix member function reference

--- test/WorkerMock.cpp
+++ test/WorkerMock.cpp
@@ -10,7 +10,7 @@ AsyncWorkerMock::AsyncWorkerMock(bool callReal) : load_(0){
 		ON_CALL(*this, blockUntilReady()).WillByDefault(Invoke(real_.get(), &AsyncWorker::blockUntilReady));
 		ON_CALL(*this, stop()).WillByDefault(Invoke(real_.get(), &AsyncWorker::stop));
 		ON_CALL(*this, doJob(_, _)).WillByDefault(
-			DoAll(WithoutArgs(Invoke(this, increaseLoad)), 
+			DoAll(WithoutArgs(Invoke(this, &AsyncWorkerMock::increaseLoad)), 
 				Invoke(real_.get(), &AsyncWorker::doJob)));
 		ON_CALL(*this, getLoad()).WillByDefault(Invoke(this, &AsyncWorkerMock::fetchLoad));
 		ON_CALL(*this, getId()).WillByDefault(Invoke(real_.get(), &AsyncWorker::getId));		
